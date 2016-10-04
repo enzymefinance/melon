4f1d491000d15da8f88e78bb4b21686a923c6fe6
retotrinkler <reto.trinkler@gmail.com>
2016-10-05 00:36:36 +0200

    implementing code review result; modifiers, style, percent_allocation

--- contracts/MelonToken.sol
+++ contracts/MelonToken.sol
@@ -17,10 +17,8 @@ contract MelonToken is ERC20, SafeMath {
     uint public constant ETHER_CAP = 500000 ether; //max amount raised during contribution (5.5M USD worth of ether will be measured with market price at beginning of the contribution)
     uint public constant TRANSFER_LOCKUP = 370285; //transfers are locked for this many blocks after endBlock (assuming 14 second blocks, this is 2 months)
     uint public constant FOUNDER_LOCKUP = 2252571; //founder allocation cannot be created until this many blocks after endBlock (assuming 14 second blocks, this is 1 year)
-    uint public constant MELONPORT_ALLOCATION = 20 * 10**16; //20% of token supply allocated post-contribution for the melonport fund
-	// ^^^ consider "uint public constant MELONPORT_PERCENT_ALLOCATION = 20;" as simpler.
-    uint public constant FOUNDER_ALLOCATION = 20 * 10**16; //20% of token supply allocated post-contribution for the founder allocation
-	// ^^^ consider "uint public constant FOUNDER_PERCENT_ALLOCATION = 20;" as simpler.
+	uint public constant MELONPORT_PERCENT_ALLOCATION = 20; //20% of token supply allocated post-contribution for the melonport fund
+	uint public constant FOUNDER_PERCENT_ALLOCATION = 20; //20% of token supply allocated post-contribution for the founder allocation
 
     // Fields that are only changed in constructor
     uint public startBlock; //contribution start block (set in constructor)
@@ -29,10 +27,10 @@ contract MelonToken is ERC20, SafeMath {
     address public signer = 0x0; // signer address (for clickwrap agreement); see function() {} for comments
 
     // Fields that can be changed by functions
+    uint public presaleEtherRaised = 0; //this will keep track of the Ether raised during the contribution
+    uint public presaleTokenSupply = 0; //this will keep track of the token supply created during the contribution
     bool public melonportAllocated = false; //this will change to true when the melonport fund is allocated
     bool public founderAllocated = false; //this will change to true when the founder fund is allocated
-    uint public presaleTokenSupply = 0; //this will keep track of the token supply created during the contribution
-    uint public presaleEtherRaised = 0; //this will keep track of the Ether raised during the contribution
     bool public halted = false; //the founder address can set this to true to halt the contribution due to emergency
 
     // EVENTS
@@ -44,28 +42,27 @@ contract MelonToken is ERC20, SafeMath {
 
     // MODIFIERS
 
-    modifier isSigner(uint8 v, bytes32 r, bytes32 s) {
+    modifier is_signer(uint8 v, bytes32 r, bytes32 s) {
         bytes32 hash = sha256(msg.sender);
         if (ecrecover(hash,v,r,s) != signer) throw;
         _
     }
 
-    modifier onlyFounder() {
+    modifier only_founder() {
         if (msg.sender != founder) throw;
         _
     }
 
-    modifier isNotHalted() {
+    modifier is_not_halted() {
         if (halted) throw;
         _
     }
 
-    modifier etherCapNotReached() {
+    modifier ether_cap_not_reached() {
         if (safeAdd(presaleEtherRaised, msg.value) > ETHER_CAP) throw;
         _
     }
 
-	// CHOOSE ONE FOR MODIFIERS: SNAKE OR CAMEL (i prefer snake for modifiers, camel for functions/fields)
     modifier block_number_at_least(uint x) {
         if (block.number >= x) throw;
         _
@@ -81,10 +78,25 @@ contract MelonToken is ERC20, SafeMath {
         _
     }
 
+    modifier when_founder_not_allocated() {
+        if (founderAllocated) throw;
+        _
+    }
+
+    modifier when_melonport_is_allocated() {
+        if (!melonportAllocated) throw;
+        _
+    }
+
+    modifier when_melonport_not_allocated() {
+        if (melonportAllocated) throw;
+        _
+    }
+
     // METHODS
 
     /// Pre: ALL fields, except { founder, signer, startBlock, endBlock } IS_VALID
-    /// Post: `founder` IS_VALID, `signer` ID_VALID, `startBlock` IS_VALID, `end_block` IS_VALID.  
+    /// Post: `founder` IS_VALID, `signer` ID_VALID, `startBlock` IS_VALID, `end_block` IS_VALID.
     function MelonToken(address founderInput, address signerInput, uint startBlockInput, uint endBlockInput) {
         founder = founderInput;
         signer = signerInput;
@@ -102,12 +114,13 @@ contract MelonToken is ERC20, SafeMath {
 
     /// Pre: startBlcok, endBlock specified in constructor
     /// Post: Contribution price in MLN/ETH; No state transition
-    function price() constant returns(uint) {
+    function price() constant returns(uint)
+    {
         if (block.number>=startBlock && block.number<startBlock+250) return 170; //power hour
         if (block.number<startBlock || block.number>endBlock) return 100; //default price
 		// ^^^ THIS MAKES NO SENSE. BETTER DEFINITION OF startBlock/endBlock REQUIRED.
         return 100 + 4*(endBlock - block.number)/(endBlock - startBlock + 1)*67/4; //contribution price
-		// ^^^ this is integer arithmetic, including division, on values within the same range - does it work properly under all circumstances? 		
+		// ^^^ this is integer arithmetic, including division, on values within the same range - does it work properly under all circumstances?
     }
 
     /// Pre: Buy entry point
@@ -119,68 +132,55 @@ contract MelonToken is ERC20, SafeMath {
     /// Pre: Buy on behalf of a recipient
     /// Post: Buy MLN, send msg.value to founder address
     function buyRecipient(address recipient, uint8 v, bytes32 r, bytes32 s)
-        isSigner(v, r, s)
+        is_signer(v, r, s)
         block_number_at_least(startBlock)
         block_number_at_most(endBlock)
-        isNotHalted()
-        etherCapNotReached()
+        is_not_halted()
+        ether_cap_not_reached()
     {
         uint tokens = safeMul(msg.value, price());
 		// ^^^ provide invariant over range of price.
         balances[recipient] = safeAdd(balances[recipient], tokens);
         totalSupply = safeAdd(totalSupply, tokens);
         presaleEtherRaised = safeAdd(presaleEtherRaised, msg.value);
-	    if (!founder.call.value(msg.value)()) throw; //immediately send Ether to founder address
-    	// ^^^ Consider the gas-bounded .send  
+        if(!founder.send(msg.value)) throw; //immediately send Ether to founder address
         Buy(recipient, msg.value, tokens);
     }
 
-    /// Pre: Founder, after freeze period plus founder lockup period is over
-    /// Post: Set up founder address token balance w founder allocation,
+    /// Pre: Fixed presaleTokenSupply. Founder, after freeze period plus founder lockup period is over
+    /// Post: Allocate funds of Founders and Advisors to founder address.
     function allocateFounderTokens()
-        onlyFounder()
+        only_founder()
         block_number_past(endBlock + FOUNDER_LOCKUP)
+        when_founder_not_allocated()
+        when_melonport_is_allocated()
     {
-        if (founderAllocated) throw;
-		// ^^^ use condition "when_founder_not_allocated"
-        if (!melonportAllocated) throw;
-		// ^^^ use condition "when_melonport_is_allocated"
-        balances[founder] = safeAdd(balances[founder], presaleTokenSupply * FOUNDER_ALLOCATION / (1 ether));
-		// ^^^ consider:
-		// var founder_allocation = presaleTokenSupply * FOUNDER_PERCENT_ALLOCATION / 100;
-		// balances[founder] = safeAdd(balances[founder], founder_allocation);
-        totalSupply = safeAdd(totalSupply, presaleTokenSupply * FOUNDER_ALLOCATION / (1 ether));
-		// ^^^ consider: 
-		// totalSupply = safeAdd(totalSupply, founder_allocation);
+		var founder_allocation = presaleTokenSupply * FOUNDER_PERCENT_ALLOCATION / 100;
+		balances[founder] = safeAdd(balances[founder], founder_allocation);
+		totalSupply = safeAdd(totalSupply, founder_allocation);
         founderAllocated = true;
         AllocateFounderTokens(msg.sender);
     }
 
-    /// Pre: Founder, after freeze period is over
-    /// Post: Set up founder address token balance w melonport allocation,
+    /// Pre: Everybody (to prevent inflation gains), after contribution period has ended.
+    /// Post: Fix presaleTokenSupply raised. Allocate funds of Melonport to founder address.
     function allocateMelonportTokens()
-        onlyFounder()
         block_number_past(endBlock)
+        when_melonport_not_allocated()
     {
-        if (melonportAllocated) throw;
-		// ^^^ use condition "when_melonport_not_allocated"
         presaleTokenSupply = totalSupply;
-        balances[founder] = safeAdd(balances[founder], presaleTokenSupply * MELONPORT_ALLOCATION / (1 ether));
-		// ^^^ consider:
-		// var melonport_allocation = presaleTokenSupply * MELONPORT_PERCENT_ALLOCATION / 100;
-		// balances[founder] = safeAdd(balances[founder], melonport_allocation);
-        totalSupply = safeAdd(totalSupply, presaleTokenSupply * MELONPORT_ALLOCATION / (1 ether));
-		// ^^^ consider: 
-		// totalSupply = safeAdd(totalSupply, melonport_allocation);
+		var melonport_allocation = presaleTokenSupply * MELONPORT_PERCENT_ALLOCATION / 100;
+		balances[founder] = safeAdd(balances[founder], melonport_allocation);
+		totalSupply = safeAdd(totalSupply, melonport_allocation);
         melonportAllocated = true;
         AllocateMelonportTokens(msg.sender);
     }
 
-    function halt() onlyFounder() { halted = true; }
+    function halt() only_founder() { halted = true; }
 
-    function unhalt() onlyFounder() { halted = false; }
+    function unhalt() only_founder() { halted = false; }
 
-    function changeFounder(address newFounder) onlyFounder() { founder = newFounder; }
+    function changeFounder(address newFounder) only_founder() { founder = newFounder; }
 
     /// Pre: Prevent transfers until freeze period is over.
     /// Post: Transfer MLN from msg.sender
