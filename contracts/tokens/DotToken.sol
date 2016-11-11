14aaa2339510bc6fbbfe4ee12d688be11723149c
retotrinkler <reto.trinkler@gmail.com>
2016-11-11 14:00:15 +0100

    simplify token contracts

--- contracts/tokens/DotToken.sol
+++ contracts/tokens/DotToken.sol
@@ -4,19 +4,5 @@ import "./MelonToken.sol";
 
 /// @title Dot Token Contract
 /// @author Melonport AG <team@melonport.com>
-contract DotToken is MelonToken {
-
-    // FILEDS
-
-    // Constant token specific fields
-    string public constant name = "DotToken";
-    string public constant symbol = "DOT";
-    uint public constant decimals = 18;
-
-    // METHODS
-
-    function DotToken(address createdBy, uint startTimeInput, uint endTimeInput)
-        MelonToken(createdBy, startTimeInput, endTimeInput)
-    {}
-
-}
+/// @notice Contract codes are identical, not however contract fields
+contract DotToken is MelonToken {}
--- truffle.js
+++ truffle.js
@@ -24,11 +24,7 @@ module.exports = {
       // from - default address to use for any transaction Truffle makes during migrations
     },
     "morden": {
-      network_id: 2 // Official Ethereum test network
-    },
-    "staging": {
-      network_id: 1337 // custom private network
-      // use default rpc settings
+      network_id: 2, // Official Ethereum test network
     },
     "development": {
       network_id: "default"
